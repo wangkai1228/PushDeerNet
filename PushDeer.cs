f003ce813b7a5d97a8bdac083143054a9382d1bb
wangkai <1125138065@qq.com>
2022-04-30 22:33:33 +0800

    更改项目输出类型为类库 更改所有函数为 async/await

--- PushDeer.cs
+++ PushDeer.cs
@@ -2,10 +2,8 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
-using Newtonsoft.Json;
 
-namespace PushDeer
+namespace PushDeerNet
 {
     /// <summary>
     /// 推送类型
@@ -16,21 +14,28 @@ namespace PushDeer
         Text,
         Image
     }
-    class PushDeerClient
+    public class PushDeer
     {
-        HttpClient httpClient;
-        public PushDeerClient(Uri baseUri)
+        private readonly HttpClient httpClient;
+        public PushDeer(Uri baseAddress)
         {
             httpClient = new HttpClient
             {
-                BaseAddress = baseUri
+                BaseAddress = baseAddress
+            };
+        }
+        public PushDeer(string baseAddress)
+        {
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
             };
         }
         /// <summary>
         /// 通过苹果 idToken 登入
         /// </summary>
         /// <param name="code">Sign in with Apple 中得到的 idToken</param>
-        public async void IDTokenLogin(string idToken)
+        public async Task<string> IDTokenLogin(string idToken)
         {
             if (httpClient != null)
             {
@@ -39,14 +44,15 @@ namespace PushDeer
                 {
                     { new StringContent(idToken), "idToken" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 通过微信 oauth code 登入
         /// </summary>
         /// <param name="code">客户端微信授权得到的code</param>
-        public async void WecodeLogin(string code)
+        public async Task<string> WecodeLogin(string code)
         {
             if (httpClient != null)
             {
@@ -55,8 +61,9 @@ namespace PushDeer
                 {
                     { new StringContent(code), "code" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 合并用户并将旧用户删除
@@ -64,7 +71,7 @@ namespace PushDeer
         /// <param name="token">认证token</param>
         /// <param name="type">字符串，必须为 apple 或者 wechat</param>
         /// <param name="tokenorcode">type 为 apple时此字段为 idToken，否则为 微信code	</param>
-        public async void MergeUser(string token, string type, string tokenorcode)
+        public async Task<string> MergeUser(string token, string type, string tokenorcode)
         {
             if (httpClient != null)
             {
@@ -75,14 +82,15 @@ namespace PushDeer
                     { new StringContent(type), "type" },
                     { new StringContent(tokenorcode), "tokenorcode" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 获得当前用户的基本信息
         /// </summary>
         /// <param name="token">认证token</param>
-        public async void GetUserInfo(string token)
+        public async Task<string> GetUserInfo(string token)
         {
             if (httpClient != null)
             {
@@ -91,8 +99,9 @@ namespace PushDeer
                 {
                     { new StringContent(token), "token" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 注册设备
@@ -101,7 +110,7 @@ namespace PushDeer
         /// <param name="name">设备名称</param>
         /// <param name="deviceToken">device token（推送用）</param>
         /// <param name="isClip">是否轻应用	0为APP</param>
-        public async void RegisterDevice(string token, string name, string deviceToken, int isClip)
+        public async Task<string> RegisterDevice(string token, string name, string deviceToken, int isClip)
         {
             if (httpClient != null)
             {
@@ -113,14 +122,15 @@ namespace PushDeer
                     { new StringContent(deviceToken), "device_id" },
                     { new StringContent(isClip.ToString()), "is_Clip" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 获取设备列表
         /// </summary>
         /// <param name="token">认证token</param>
-        public async void GetDeviceList(string token)
+        public async Task<string> GetDeviceList(string token)
         {
             if (httpClient != null)
             {
@@ -129,8 +139,9 @@ namespace PushDeer
                 {
                     { new StringContent(token), "token" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 重命名设备
@@ -138,7 +149,7 @@ namespace PushDeer
         /// <param name="token">认证token	
         /// <param name="deviceID">设备ID</param>
         /// <param name="newName">设备新名称</param>
-        public async void RenameDevice(string token, int deviceID, string newName)
+        public async Task<string> RenameDevice(string token, int deviceID, string newName)
         {
             if (httpClient != null)
             {
@@ -149,8 +160,9 @@ namespace PushDeer
                     { new StringContent(deviceID.ToString()), "id" },
                     { new StringContent(newName), "name" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 移除设备
@@ -158,7 +170,7 @@ namespace PushDeer
         /// <param name="token">认证token	
         /// <param name="deviceID">设备ID</param>
         /// <param name="newName">设备新名称</param>
-        public async void RemoveDevice(string token, int deviceID)
+        public async Task<string> RemoveDevice(string token, int deviceID)
         {
             if (httpClient != null)
             {
@@ -168,14 +180,15 @@ namespace PushDeer
                     { new StringContent(token), "token" },
                     { new StringContent(deviceID.ToString()), "id" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 生成一个新Key
         /// </summary>
         /// <param name="token">认证token</param>
-        public async void GenerateKey(string token)
+        public async Task<string> GenerateKey(string token)
         {
             if (httpClient != null)
             {
@@ -184,8 +197,9 @@ namespace PushDeer
                 {
                     { new StringContent(token), "token" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 重命名Key
@@ -193,7 +207,7 @@ namespace PushDeer
         /// <param name="token">认证token</param>
         /// <param name="keyID">Key ID</param>
         /// <param name="newKeyName">Key新名称</param>
-        public async void RenameKey(string token, int keyID, string newKeyName)
+        public async Task<string> RenameKey(string token, int keyID, string newKeyName)
         {
             if (httpClient != null)
             {
@@ -204,15 +218,16 @@ namespace PushDeer
                     { new StringContent(keyID.ToString()), "id" },
                     { new StringContent(newKeyName), "name" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 重置一个Key
         /// </summary>
         /// <param name="token">认证token</param>
         /// <param name="keyID">Key ID</param>
-        public async void RegenKey(string token, int keyID)
+        public async Task<string> RegenKey(string token, int keyID)
         {
             if (httpClient != null)
             {
@@ -222,14 +237,15 @@ namespace PushDeer
                     { new StringContent(token), "token" },
                     { new StringContent(keyID.ToString()), "id" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 获取当前用户的Key列表
         /// </summary>
         /// <param name="token">认证token</param>
-        public async void GetKeyList(string token)
+        public async Task<string> GetKeyList(string token)
         {
             if (httpClient != null)
             {
@@ -238,15 +254,16 @@ namespace PushDeer
                 {
                     { new StringContent(token), "token" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 删除Key
         /// </summary>
         /// <param name="token">认证token</param>
         /// <param name="keyID">Key ID</param>
-        public async void RemoveKey(string token, int keyID)
+        public async Task<string> RemoveKey(string token, int keyID)
         {
             if (httpClient != null)
             {
@@ -256,8 +273,9 @@ namespace PushDeer
                     { new StringContent(token), "token" },
                     { new StringContent(keyID.ToString()), "id" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 推送消息
@@ -266,7 +284,7 @@ namespace PushDeer
         /// <param name="text">推送消息内容</param>
         /// <param name="desp">消息内容第二部分，选填</param>
         /// <param name="type">格式，选填</param>
-        public async void PushMessage(string pushkey, string text, string desp = "", PushType type = PushType.Text)
+        public async Task<string> PushMessage(string pushkey, string text, string desp = "", PushType type = PushType.Text)
         {
             if (httpClient != null)
             {
@@ -278,8 +296,9 @@ namespace PushDeer
                     { new StringContent(desp), "desp" },
                     { new StringContent(type.ToString()), "type" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 获得当前用户的消息列表
@@ -303,7 +322,7 @@ namespace PushDeer
         /// </summary>
         /// <param name="token">认证token</param>
         /// <param name="limit">消息条数	默认为10，最大100</param>
-        public async void GetMessageList(string token, int limit = 10)
+        public async Task<string> GetMessageList(string token, int limit = 10)
         {
             if (limit > 100)
             {
@@ -317,15 +336,16 @@ namespace PushDeer
                     { new StringContent(token), "token" },
                     { new StringContent(limit.ToString()), "limit" }
                 };
-                await SendRequest(newUri, multipartFormData);
+                return await SendRequest(newUri, multipartFormData);
             }
+            return null;
         }
         /// <summary>
         /// 删除消息
         /// </summary>
         /// <param name="token">认证token</param>
         /// <param name="messageID">消息ID</param>
-        public async void RemoveMessage(string token, int messageID)
+        public async Task<string> RemoveMessage(string token, int messageID)
         {
             if (httpClient != null)
             {
@@ -335,34 +355,9 @@ namespace PushDeer
                     { new StringContent(token), "token" },
                     { new StringContent(messageID.ToString()), "id" }
                 };
-                string result = await SendRequest(newUri, multipartFormData);
-                RemoveResult removeResult = JsonConvert.DeserializeObject<RemoveResult>(result);
-                if (removeResult != null && removeResult.code == 0)
-                {
-                    Console.WriteLine($"移除||MessageID = {messageID}||Message = {removeResult.content.message}||成功");
-                }
+                return await SendRequest(newUri, multipartFormData);
             }
-        }
-        /// <summary>
-        /// 清空所有的消息
-        /// </summary>
-        /// <param name="token">认证token</param>
-        public async void ClearAllMessage(string token)
-        {
-            string result = await GetMessageList(token);
-            Console.WriteLine($"{result}");
-            MessagesResult messages = JsonConvert.DeserializeObject<MessagesResult>(result);
-            if (messages.code == 0)
-            {
-                Console.WriteLine("获取成功！！！");
-                if (messages.content.messages.Count > 0)
-                {
-                    for (int i = 0; i < messages.content.messages.Count; i++)
-                    {
-                        RemoveMessage(token, messages.content.messages[i].id);
-                    }
-                }
-            }
+            return null;
         }
         /// <summary>
         /// 发送请求
